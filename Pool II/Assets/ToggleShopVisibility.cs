0c55265690d2af352ed95dab46d380436ba6af63
ryanakann <ryanakann@gmail.com>
2020-08-15 15:50:06 -0400

    Shop UI!

---
+++ Pool II/Assets/ToggleShopVisibility.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleShopVisibility : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform rectTransform = null;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float damping = 0.2f;
+
+    private Vector2 positionSmoothing;
+    private bool open;
+    private Vector2 target;
+
+    private void Start()
+    {
+        open = false;
+        target = Vector2.zero;
+    }
+
+    public void Toggle()
+    {
+        open = !open;
+        target = open ? Vector2.right * 128 : Vector2.zero;
+    }
+
+    private void Update()
+    {
+        SetPositionOfPivot(Vector2.SmoothDamp(rectTransform.anchoredPosition, target, ref positionSmoothing, damping));
+    }
+
+    private void SetPositionOfPivot(Vector2 newPos)
+    {
+        rectTransform.anchoredPosition = newPos;
+    }
+}
