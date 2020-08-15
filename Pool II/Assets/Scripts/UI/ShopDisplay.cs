ab0b81e9572d52d1fbbb2e56fe37959dae3c0459
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-08-15 18:05:30 -0400

    populate shop with items, but it isn't styled right yet

--- Pool II/Assets/Scripts/UI/ShopDisplay.cs
+++ Pool II/Assets/Scripts/UI/ShopDisplay.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopDisplay : MonoBehaviour {
     [SerializeField]
     EconomyManager EM;
+    [SerializeField]
+    GameObject ShopItemUI;
+
     void Start() {
         foreach (ShopItem item in EM.GetShopItems()) {
             print(item.name);
-            //update UI to include button for item
+            UIForShopItem(item);
         }
+    }
+
+    GameObject UIForShopItem(ShopItem item) {
+        GameObject itemUI = Instantiate(ShopItemUI, transform.FindDeepChild("Content"));
+        itemUI.GetComponent<Image>().sprite = item.icon;
+        itemUI.transform.Find("ItemName").GetComponent<TextMeshProUGUI>().text = item.name.ToUpper();
+        return itemUI;
     }
 }
