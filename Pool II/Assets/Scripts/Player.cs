d7b2ed7eeabf5f02349878039a4ad7f6d8387452
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2020-08-15 15:51:18 -0400

    turns even

---
+++ Pool II/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [HideInInspector]
+    bool myTurn = false;
+
+    public void StartTurn()
+    {
+        print(gameObject + ":");
+        print("It's my turn");
+        myTurn = true;
+    }
+
+    public void EndTurn()
+    {
+        print("Ending turn!");
+        myTurn = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Pool II/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    List<Player> players = new List<Player>();
+    int currentPlayer;
+
+    public static TurnManager instance;
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        players = new List<Player>(FindObjectsOfType<Player>());
+        currentPlayer = Random.Range(0, players.Count);
+
+        NextTurn(true);
+    }
+
+    public void NextTurn(bool first = false)
+    {
+        if (!first) players[currentPlayer].EndTurn();
+        players[currentPlayer].StartTurn();
+        currentPlayer = ++currentPlayer % players.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            NextTurn();
+        }
+    }
+}
