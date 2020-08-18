eb31fcd6d149642b5ab007d8d6555754f4e378d6
SimonMarkWhittle <simonmarkwhittle@gmail.com>
2020-08-17 22:25:47 -0400

    Added basic ExplodaBall logic

---
+++ Pool II/Assets/Scripts/Balls/ExplodaBall.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodaBall : BaseBall
+{
+    [SerializeField]
+    protected float radius = 5.0F;
+    [SerializeField]
+    protected float power = 10.0F;
+
+    bool fuse_lit = false;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+    }
+
+    protected override void OnHitByOtherBall()
+    {
+        base.OnHitByOtherBall();
+        fuse_lit = true;
+    }
+
+    protected override void OnHitOtherBall()
+    {
+        base.OnHitOtherBall();
+
+        if (fuse_lit)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+        }
+    }
+
+}
