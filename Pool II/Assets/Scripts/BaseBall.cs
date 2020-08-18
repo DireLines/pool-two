4555837bd422c068fdee1bff43912134b8997d1a
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2020-08-17 23:07:07 -0400

    hoo hoo

--- Pool II/Assets/Scripts/BaseBall.cs
+++ Pool II/Assets/Scripts/BaseBall.cs
@@ -48,7 +48,7 @@ public class BaseBall : MonoBehaviour {
     }
 
     protected virtual void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.HasTag(Tag.Ball)) {//TODO: if other thing is ball
+        if (collision.gameObject.HasTag(Tag.Ball)) {
             if (!moving) {
                 OnHitByOtherBall();
             } else {
