8e3c18d0a01d910c7c03419b8ef6c5726d4deb0a
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2020-08-17 21:57:59 -0400

    taggy waggy

---
+++ Pool II/Assets/Scripts/General/TagHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Tag { None };
+
+public class TagHandler : MonoBehaviour
+{
+    public List<Tag> tags = new List<Tag>();
+}
+
+public static class TagExtension
+{
+    public static bool HasTag(this GameObject g, Tag tag)
+    {
+        TagHandler handler = g.GetComponent<TagHandler>();
+        return (handler) ? handler.tags.Contains(tag) : false;
+    }
+
+    public static bool HasTags(this GameObject g, List<Tag> tags)
+    {
+        TagHandler handler = g.GetComponent<TagHandler>();
+        foreach (Tag tag in tags)
+            if (!handler.tags.Contains(tag))
+                return false;
+        return true;
+    }
+
+    public static bool HasAnyTag(this GameObject g, List<Tag> tags)
+    {
+        TagHandler handler = g.GetComponent<TagHandler>();
+        foreach (Tag tag in tags)
+            if (handler.tags.Contains(tag))
+                return true;
+        return false;
+    }
+}
