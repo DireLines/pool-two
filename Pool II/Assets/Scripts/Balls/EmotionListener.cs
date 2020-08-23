c905ed0b3c4d4349ecf36db81bb080b318a1468a
Julián <jaykmcclinton@gmail.com>
2020-08-23 17:14:34 -0400

    Cueball emotion animations

---
+++ Pool II/Assets/Scripts/Balls/EmotionListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Emotion {
+    Idle,
+    Mad,
+    Sad,
+    Had,
+}
+
+//public delegate void EmotiveEvent(Emotion emotion);
+
+public class EmotionListener : MonoBehaviour {
+    //public event EmotiveEvent Emote;
+
+    Animator anim;
+
+    private void Start() {
+        anim = GetComponentInChildren<Animator>();
+        //Emote += OnListen;
+    }
+
+    public void OnListen(Emotion emotion) {
+        anim.SetInteger("emotion", (int)emotion);
+    }
+}
